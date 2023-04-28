4157877e55fdee5d43720993bc7dd035054d14ee
blackmarllbor0 <blackmarllbor07@gmail.com>
2023-04-28 11:55:54 +0300

    Now it's possible to use both UI and if you have one question, then you can simply use `-q` parameter to ask it without calling the UI.

--- cmd/terminalGPT.go
+++ cmd/terminalGPT.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"fmt"
 	"github.com/spf13/viper"
 	"log"
 	"terminalGPT/config"
+	"terminalGPT/internal/app/flags"
 	"terminalGPT/internal/pkg/api/GPT3"
 	"terminalGPT/pkg/dir/yaml"
 	"terminalGPT/pkg/ui"
@@ -19,8 +21,29 @@ func main() {
 
 	gptModel := GPT3.NewGPT3(configReaderService)
 
-	userInterface := ui.NewUserInterface(gptModel)
-	if err := userInterface.Start(); err != nil {
-		log.Fatal(err)
+	query, newApiKey := flags.Parse()
+	if query == "" && newApiKey == "" {
+		userInterface := ui.NewUserInterface(gptModel)
+		if err := userInterface.Start(); err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		if newApiKey != "" {
+			if err := configService.SetApiKey(newApiKey); err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println("Api key is successfully modified")
+			return
+		}
+		if query != "" {
+			response, err := gptModel.GenerateText(query)
+			if err != nil {
+				log.Fatal(err)
+			}
+
+			fmt.Println(response)
+			return
+		}
 	}
 }
--- internal/app/flags/flags.go
+++ internal/app/flags/flags.go
@@ -2,35 +2,11 @@ package flags
 
 import "flag"
 
-type Flags struct{}
-
-func NewFlags() *Flags {
-	return &Flags{}
-}
-
-func (f *Flags) Parse() (query, apiKey string) {
+func Parse() (query, apiKey string) {
 	flag.StringVar(&query, "q", "", "")
 	flag.StringVar(&apiKey, "api-key", "", "")
 
 	flag.Parse()
 
 	return
-}
-
-func (f *Flags) GetQuery() string {
-	var query string
-	flag.StringVar(&query, "q", "", "")
-
-	flag.Parse()
-
-	return query
-}
-
-func (f *Flags) SetApiKey() string {
-	var apiKey string
-	flag.StringVar(&apiKey, "api-key", "", "")
-
-	flag.Parse()
-
-	return apiKey
 }
